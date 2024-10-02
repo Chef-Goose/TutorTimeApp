081d1d37f9e5a02647db42d1d008ee086b740ab6
Chef-Goose <chefgoose69@gmail.com>
2024-10-01 22:18:07 -0600

    Login and Signup Screen created, buttons not connected

---
+++ app/src/main/java/com/example/tutorapp/LoginActivity.kt
@@ -0,0 +1,28 @@
+package com.example.tutorapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnSignIn = findViewById<Button>(R.id.btnSignIn)
+        btnSignIn.setOnClickListener {
+            val Intent = Intent(this,SignUpActivity::class.java)
+            startActivity(Intent)
+        }
+    }
+
+}

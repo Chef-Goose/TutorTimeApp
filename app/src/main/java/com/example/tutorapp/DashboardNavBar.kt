387b17f54c833e783e1c92fb148b01747573fe62
RyanJay264 <Rosalesr6@mymacewan.ca>
2024-10-04 22:58:37 -0600

    Added dashboard and Nav

--- app/src/main/java/com/example/tutorapp/DashboardNavBar.kt
+++ app/src/main/java/com/example/tutorapp/DashboardNavBar.kt
@@ -3,6 +3,7 @@ package com.example.tutorapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,5 +20,16 @@ class DashboardNavBar : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val btnHomeScr = findViewById<ImageButton>(R.id.home_button)
+        btnHomeScr.setOnClickListener {
+            val intent = Intent(this@DashboardNavBar,DashboardNavBar::class.java)
+            startActivity(intent)
+        }
+        val btnSignOut = findViewById<ImageButton>(R.id.sign_out_button)
+        btnSignOut.setOnClickListener {
+            val intent = Intent(this@DashboardNavBar,LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }

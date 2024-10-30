6c8cfbfa9714141cc74686ccf0e36a47d770a88e
Chef-Goose <chefgoose69@gmail.com>
2024-10-30 01:50:09 -0600

    firebase offline usage

--- app/src/main/java/com/example/tutorapp/MainActivity.kt
+++ app/src/main/java/com/example/tutorapp/MainActivity.kt
@@ -3,30 +3,26 @@ package com.example.tutorapp
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.database.FirebaseDatabase
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.example.tutorapp.databinding.ActivityMainBinding
 import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Enable offline persistence for Firebase Realtime Database
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true)
 
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+
+        // Delay to show splash screen and then navigate to LoginActivity
         Handler().postDelayed({
             val intent = Intent(this@MainActivity, LoginActivity::class.java)
             startActivity(intent)
             finish()
-        },3000)
+        }, 3000)
     }
-
 }

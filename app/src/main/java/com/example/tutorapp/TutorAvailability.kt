b114c0e90e04a8c48d0ea5321e6565a3789382b7
RyanJay264 <150179090+RyanJay264@users.noreply.github.com>
2024-11-07 04:04:58 -0700

    Added RealTime chat system

---
+++ app/src/main/java/com/example/tutorapp/TutorAvailability.kt
@@ -0,0 +1,9 @@
+package com.example.tutorapp
+
+data class TutorAvailability(
+    val course: String = "",
+    val timeSlot: String = "",
+    val certificate: String = "",
+    val date: Long = 0L,
+    val name: String = "" // Add tutor name
+)

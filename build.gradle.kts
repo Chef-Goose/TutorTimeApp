87ccb8b8ec6d981482c281b7e55fc546f156e4bd
Chef-Goose <150098211+Chef-Goose@users.noreply.github.com>
2025-10-21 02:09:37 -0600

    fixing secret breaches

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+import java.util.Properties
+
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    val properties = Properties()
+    properties.load(localPropertiesFile.inputStream())
+    properties.forEach { (key, value) ->
+        project.extensions.extraProperties.set(key.toString(), value)
+    }
+}
+
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
